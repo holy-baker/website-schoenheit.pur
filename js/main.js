ab566803746864be65445c68994b094f1a404799
holy-baker <147312260+holy-baker@users.noreply.github.com>
2023-11-03 19:00:04 +0100

    20231103
    parallax-effect angepasst

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 function myFunction() {
-    var x = document.getElementById("myNavbar");
-    if (x.className === "navbar") {
-      x.className += " responsive";
-    } else {
-      x.className = "navbar";
-    }
+  var x = document.getElementById("myNavbar");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
   }
+}
 
 
 
@@ -13,20 +13,13 @@ function myFunction() {
 const parallax = document.getElementById("parallax-1");
 
 window.addEventListener('scroll', function () {
-    let offset = this.window.pageYOffset;
-
-    elements.forEach((element) => {
-    let offset = (element.offsetTop - (window.pageYOffset)) * 0.8
-    element.style.backgroundPositionY = `${offset}px`;
-    });
-});
-
-
-
-
-
-
+    
+    // let offset = this.window.pageYOffset;
     // elements.forEach((element) => {
-    //   let offset = (element.offsetTop - (window.pageYOffset)) * 0.8
-    //   element.style.backgroundPositionY = `${offset}px`;
+    // let offset = (element.offsetTop - (window.pageYOffset)) * 0.8
+    // element.style.backgroundPositionY = `${offset}px`;
     // });
+
+
+  
+});
